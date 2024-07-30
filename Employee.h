c4449256811203a270f26dd98d952f63d5e947c5
mayuejan <ray.mar@icloud.com>
2024-07-30 11:47:57 -0400

    Update Employee.h

--- Employee.h
+++ Employee.h
@@ -1,19 +1,5 @@
 /**
 * File Name: Employee.h
-*
-* Academic Integrity Statement:
-* I certify that, while others may have assisted me in brainstorming, debugging, and validating this program,
-* the program itself is my own work. I understand that submitting code that is the work of other individuals
-* is a violation of the course Academic Integrity Policy and may result in zero credit for the assignment, or
-* course failure and a report to the Academic Dishonesty Board. I also understand that if I knowingly give my
-* original work to another individual could also result in zero credit for the assignment, or course failure
-* and a report to the Academic Dishonesty Board. See Academic Integrity for Students
-Links to an external site.
-*
-* Assisted by and Assisted line numbers:
-* Your Name: YUERAN MA
-* Your PSU user ID:  ymm5252
-* Course title: CMPSC122 Summer 2024
 * Description: This is a skeleton codes for employee class definition
  * File: Employee.h
  * Skeleton codes for Employee class definition.
